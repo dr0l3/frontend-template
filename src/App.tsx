cba486a25c2203e3570c60b10eede9045a4e51c7
dr0l3 <runedrolepedersen@gmail.com>
2019-07-14 22:00:50 +0200

    Working integration with semantic ui react

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Query } from 'react-apollo'
 import './App.css';
 import listUsersQuery from './queries/listUsersQuery'
 import { listUsers, listUsers_users } from './queries/types/listUsers'
+import { Card, Icon, Image } from 'semantic-ui-react'
+
 
 class UsersQuery extends Query<listUsers, null> {}
 
@@ -11,7 +13,7 @@ const App: React.FC = () => {
   return (
     <UsersQuery query={listUsersQuery}>
       {({data, loading,error}) => (
-        <React.Fragment>
+        /*<React.Fragment>
           <h1> Users </h1>
           {data && data.users && data.users.map(user => (
             <div>
@@ -19,6 +21,20 @@ const App: React.FC = () => {
               <div>Email: {user && user.email}</div>
             </div>
           ))}
+        </React.Fragment>*/
+        <React.Fragment>
+        {data && data.users && data.users.map(user => (
+          <Card>
+            <Card.Content>
+              <Card.Header>
+                {user && user.name}
+              </Card.Header>
+            </Card.Content>
+            <Card.Description>
+              Email: {user && user.email}
+            </Card.Description>
+          </Card>
+        ))}
         </React.Fragment>
       )
     }
